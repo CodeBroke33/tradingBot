f619db71d606772ec8c09d8d68190a8cbffbf1a7
CodeBroke33 <minedom33@gmail.com>
2024-02-12 16:21:40 +0200

    update

--- tradingbot.py
+++ tradingbot.py
@@ -4,12 +4,31 @@ from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
 
-API_KEY ""
-API_SECRET = ""
-BASE_URL = ""
+API_KEY = "PK58SMNRXQUVZWVYVGB2"
+API_SECRET = "TbB2nHS8bAaoNm1ZKH3fjhz1n4HcnA2rzTFk0QqL"
+BASE_URL = "https://paper-api.alpaca.markets"
 
 ALPACA_CREDS = {
-    "API_KEY":API_KEY
-    "API_SECRET": API_SECRET,
-    "PAPER": True
+    "API_KEY":API_KEY,
+    "API_SECRET": API_SECRET,  
+    "PAPER": True 
 }
+
+class MLTrader(Strategy):
+    def initialize(self, ):
+        pass
+    def on_trading_iteration(self)
+            pass
+
+start_date = datetime(2024,2,15)
+end_date = datetime(2024,3,31)
+broker = Alpaca(ALPACA_CREDS)
+Strategy = MLTrader(name='mlstrat', broker=broker, 
+                   parameters={})
+    
+    strategy.backtesting(
+         YahoodataBacktesting,
+         start_date,
+         end_date,
+         parameters={} 
+    )
