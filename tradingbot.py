45b60216a88b773c2b68451bb8455cfa50fe91fe
CodeBroke33 <minedom33@gmail.com>
2024-02-23 11:02:34 +0200

    update

--- tradingbot.py
+++ tradingbot.py
@@ -15,20 +15,31 @@ ALPACA_CREDS = {
 }
 
 class MLTrader(Strategy):
-    def initialize(self, ):
-        pass
-    def on_trading_iteration(self)
-            pass
+    def initialize(self,symbol:str="SPY"):
+            self.symbol = symbol
+            self.sleeptime = "24H"
+            self.last_trade = None
+
+    def on_trading_iteration(self):
+            self.last_trade == None,
+            order = self.create_order(
+                   self.symbol,
+                   10,
+                   "buy",
+                   type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
 
 start_date = datetime(2024,2,15)
 end_date = datetime(2024,3,31)
 broker = Alpaca(ALPACA_CREDS)
 Strategy = MLTrader(name='mlstrat', broker=broker, 
-                   parameters={})
+                   parameters={"symbol":"SPY"})
     
-    strategy.backtesting(
+strategy.backtesting(
          YahoodataBacktesting,
          start_date,
          end_date,
-         parameters={} 
+         parameters={"symbol":"SPY"} 
     )
